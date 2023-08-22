41ee9d693a2a1593ec4182779b9dbaa9fafba556
Blue Endaya <blue.endaya@gmail.com>
2023-08-22 18:24:50 +0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://loginuser:sup3rman1234@cluster0.ohhevlp.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "9SudtUMi+QBLUlHR729l64Y/YyH2n+5XVq0fM+nT8brYnba7CBo2jfSBZCyOoWjmjtLPqKfA1tvmg8uCC+2NXIYX3zguzA=="
+  };
